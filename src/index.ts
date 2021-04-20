8195afa3579ec92bd5ecb20aec9c80b5ac6dc31d
Djura22 <michaeliangroves@gmail.com>
2021-04-20 11:36:22 +0100

    Build: Simple Express func's in main for port set and send (res)

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,25 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
+import express from "express";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   orm.getMigrator().up();
 
+  const app = express();
+  app.get('/', (_, res) => {
+    res.send('Hello');
+  });
+  app.listen(4000, () => {
+    console.log('Server started on port localhost:4000')
+  });
+  
   // const post = orm.em.create(Post, {title: 'My first post'});
   // await orm.em.persistAndFlush(post);
 
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts);
 
 };
 
