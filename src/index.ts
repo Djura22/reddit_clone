fa57e5c22b0613218e483cbf7257efcbdd24a243
Djura22 <michaeliangroves@gmail.com>
2021-04-20 11:47:42 +0100

    Build: GraphQL schema setup on main using apolloServer with Resolver class hello.ts

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,33 @@ import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from "./mikro-orm.config";
 import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   orm.getMigrator().up();
 
   const app = express();
-  app.get('/', (_, res) => {
-    res.send('Hello');
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    })
   });
+
+  apolloServer.applyMiddleware({ app });
+
+  // app.get('/', (_, res) => {
+  //   res.send('Hello');
+  // });
+
   app.listen(4000, () => {
     console.log('Server started on port localhost:4000')
   });
-  
+
   // const post = orm.em.create(Post, {title: 'My first post'});
   // await orm.em.persistAndFlush(post);
 
